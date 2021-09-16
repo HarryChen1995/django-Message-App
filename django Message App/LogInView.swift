2fa54ab4040e7a556e795aeb70148ccd8fef4a48
harrychen1995 <harrychen1995@yahoo.com>
2021-09-15 21:56:59 -0400

    sign button added and textfield border style changed

--- django Message App/LogInView.swift
+++ django Message App/LogInView.swift
@@ -18,25 +18,29 @@ struct LogInView: View {
             VStack {
                 Image("django").resizable().frame(width: 200, height: 80).cornerRadius(7).shadow(radius: 5).offset( y: -10)
                 VStack{
-                    HStack{
-                        Text("Username:")
-                        TextField("UserName", text: $username, prompt: Text("Enter your username"))
-                    }.padding()
-                    Divider()
-                    HStack{
-                        Text("Password:")
-                        SecureField("Password", text: $password, prompt: Text("Enter your password"))
-                    }.padding().padding(.bottom, 3)
+                    
+           
+                    TextField("UserName", text: $username, prompt: Text("User Name")).textFieldStyle(.roundedBorder).padding(.bottom, 3)
+                  
+                  
+                    SecureField("Password", text: $password, prompt: Text("Password")).textFieldStyle(.roundedBorder).padding(.bottom, 3)
+                   
                     
                     Button(action: {
                         let _ = Task {
                             await sessionManger.signIn(username: username, password: password)
                         }
                     }, label: {
-                        Text("Sign In").fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: 30).padding(8).background(Color.blue).cornerRadius(10)
+                        Text("Sign In").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: 25).padding(8).background(Color.blue).cornerRadius(10)
                     })
+                    Divider()
+                    Button(action: {}, label: {
+                        Text("Create New Account").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: 25).padding(8).background(Color.green).cornerRadius(10)
+                    })
+                    
+                
                 }.padding().background(.thinMaterial).cornerRadius(14).shadow(radius: 5).padding()
-            }.padding()
+            }.font(.system(size: 14 , weight: .bold, design: .default)).padding()
         }
     }
 }
