52efabb6bae03f3ea6588140409f21ef151e55e9
harrychen1995 <harrychen1995@yahoo.com>
2021-11-08 21:10:03 -0500

    unit test case added

---
+++ django Message AppTests/django_Message_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  django_Message_AppTests.swift
+//  django Message AppTests
+//
+//  Created by Hanlin Chen on 11/8/21.
+//
+
+import XCTest
+
+class django_Message_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(1, 1)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
